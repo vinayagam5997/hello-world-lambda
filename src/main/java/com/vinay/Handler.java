450b5280ee52fafae9f49ba7f21c5d3ecd438276
Vinay <vinayagam5997@gmail.com>
2026-05-25 23:11:37 +0530

    buildspec.yml updated

--- src/main/java/com/vinay/Handler.java
+++ src/main/java/com/vinay/Handler.java
@@ -2,31 +2,12 @@ package com.vinay;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-import java.util.Map;
-
-public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class Handler implements RequestHandler<Object, String> {
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-        context.getLogger().log("Received request: " + request.getHttpMethod() + " " + request.getPath());
+    public String handleRequest(Object input, Context context) {
 
-        String name = null;
-
-        // Try query string params first
-        if (request.getQueryStringParameters() != null) {
-            name = request.getQueryStringParameters().get("name");
-        }
-
-        String message = (name != null && !name.isBlank())
-                ? "Hello, " + name + "!"
-                : "Hello, World!";
-
-        return new APIGatewayProxyResponseEvent()
-                .withStatusCode(200)
-                .withHeaders(Map.of("Content-Type", "application/json"))
-                .withBody("{\"message\": \"" + message + "\"}");
+        return "Hello From CI CD Pipeline";
     }
 }
